eced2ef88b9f4417a52e0a2b40506eb9729f8dec
abinsaji2008 <abinsajiweb@gmail.com>
2026-09-24 06:55:39 +0530

    Add cookie-based Chat2API core

---
+++ api/models.py
@@ -0,0 +1,8 @@
+model_proxy = {
+    "gpt-4o": "gpt-4o",
+    "gpt-4o-mini": "gpt-4o-mini",
+    "gpt-3.5-turbo": "gpt-3.5-turbo",
+    "o3-mini": "o3-mini",
+    "o1": "o1",
+}
+model_system_fingerprint = {}
